65e4c35b575d41c5fcf31df799566c073c622e13
altermatics <152564628+Altermatics@users.noreply.github.com>
2025-01-27 23:58:02 +0000

    completed first 4 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,39 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let inputArray = input.split('');
+    let inputArrayReverse = inputArray.reverse();
+    let reverseInput = inputArrayReverse.join('');
+    return reverseInput
 };
+
+/*
+text = hello there
+for the length of the value
+take each index of value
+take inedx [-1] and put into new array
+use shift to put index at beginning
+split string into array
+take length of value sarting from end and put into new array
+
+
+Use shift() to take first value and put into a new aray
+*/
+
+/*
+
+function reverseTest(input) {
+    let inputLength = input.length -1;
+  for (i =0; i < inputLength; i++) {
+    let lastChar = input[inputLength];
+    let reverseInput = []
+    reverseInput.push(lastChar)
+    reverseInput.toString();
+    console.log(reverseInput)
+  }
+}
+
+reverseTest('supernatural')
+
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-};
+/*
+
+Using ...args this is just comparing the values in  2 arrays
+*/
+
+const removeFromArray = function(array, ...args) {
+    let newArr = []
+    for (i = 0; i < array.length; i++) {
+        if (!args.includes(array[i])){
+        newArr.push(array[i])
+        } else {
+          // newArr.push(args[i])
+        }
+      } 
+       return newArr
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
